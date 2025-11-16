5c560f7fad985cb2181faa82b061022d2bfcb947
oliolioli <oli.meier@bluewin.ch>
2025-11-16 14:42:21 +0100

    Add ex 2.6

--- Prolog/s08_exercise2.pl
+++ Prolog/s08_exercise2.pl
@@ -29,3 +29,33 @@ my_append_rec([], Acc, Acc).      % AccIn = AccOut (= Unification)
 last_element([X], X).          % X is the last element if the list has exactly one element
 last_element([_|Tail], X) :-   % otherwise, skip the head and check the tail
     last_element(Tail, X).
+
+
+% my_select(?E,+L1,?L2)
+my_select(E, L1, L2) :-
+    my_select_rec(E, L1, [], L2).
+
+% Base case: empty list
+my_select_rec(_, [], Acc, Acc).
+
+% Recursive case: Head = E, skip it
+my_select_rec(E, [E|Tail], Acc, Result) :-
+    my_select_rec(E, Tail, Acc, Result).
+
+% Recursive case: Head \= E, include it
+my_select_rec(E, [Head|Tail], Acc, Result) :-
+    Head \= E,
+    append(Acc, [Head], NewAcc),
+    my_select_rec(E, Tail, NewAcc, Result).
+
+
+% my_sum_list(+L,?S)
+my_sum_list(L, S) :-
+  my_sum_list_rec(L, 0, S).
+
+my_sum_list_rec([Head|Tail], Acc, S) :-
+  NewAcc is Acc + Head,
+  my_sum_list_rec(Tail, NewAcc, S).
+
+my_sum_list_rec([], Acc, Acc).  % Base case with binding
+
