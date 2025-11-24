80a1970f0bda6ccf44141875b063f0c8d0e8cb79
oliolioli <oli.meier@bluewin.ch>
2025-11-24 14:47:21 +0100

    Improve edge cases.

---
+++ Prolog/s09_ex1.pl
@@ -0,0 +1,62 @@
+%S09 - Exercise 1
+
+%scalar_product(++L1,++L2,-P)
+scalar_product([H1|T1], [H2|T2], P) :-
+  TemporaryResult is H1*H2,
+  scalar_product(T1, T2, Result),
+  P is TemporaryResult + Result.
+
+scalar_product([],[],0).
+
+%scalar_product_acc(++L1,++L2,++Acc,-P)
+scalar_product_acc([H1|T1], [H2|T2], Acc, P) :-
+  TemporaryResult is H1*H2,
+  NewAcc is Acc + TemporaryResult,
+  scalar_product_acc(T1, T2, NewAcc, P).
+
+scalar_product_acc([], [], Acc, Acc).
+
+
+%fibonacci(++N,-FN), 
+fibonacci(0, 0) :- !.
+fibonacci(1, 1) :- !.
+  
+fibonacci(N,FN) :-
+  N1 is N-1,
+  N2 is N-2,
+  fibonacci(N1, FN1),
+  fibonacci(N2, FN2),
+  FN is FN1 + FN2.
+
+
+% fibonacci_acc(++N, ++Prev, ++Acc, -FN)
+fibonacci_acc(0, _, Acc, Acc) :- !.  % return Acc, not Prev
+
+fibonacci_acc(N, Prev, Acc, FN) :-
+    NewAcc is Prev + Acc,
+    N1 is N - 1,
+    fibonacci_acc(N1, Acc, NewAcc, FN).
+% Call this function as follows: fibonacci_acc(N, 0, 1, FN). Whereas N is an integer.
+
+
+%count_occur(+X,+L,-N)
+count_occur(X, [Head|Tail], N) :-
+  count_occur(X, Tail, M),
+    (   var(Head)
+    ->  N is M + 1
+    ;   (Head == X -> N is M + 1 ; N = M)         % this is needed to pass the test with 5 empty arguments.
+    ).
+
+count_occur(_, [], 0).
+
+% Call this function as follows: count_occur(X, [a,b,c], N). Whereas X: list item to be counted and [a,b,c] a list.
+
+%count_occur_acc(+X,+L,++Acc,-N)
+count_occur_acc(X, [Head|Tail], Acc ,N) :-
+  (   var(Head)
+  ->  NewAcc is Acc + 1
+  ;   (Head == X -> NewAcc is Acc + 1 ; NewAcc = Acc)
+  ),
+  count_occur_acc(X, Tail, NewAcc, N).
+
+count_occur_acc(_, [], Acc, Acc).
